f4072bf69e425bea117b286712597a6ce4d156c4
KananMV <kenan2002memmedov@gmail.com>
2024-08-31 10:41:32 +0400

    Initial Commit

---
+++ task8_cv&tv/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task8_cv&tv
+//
+//  Created by Kenan Memmedov on 31.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
